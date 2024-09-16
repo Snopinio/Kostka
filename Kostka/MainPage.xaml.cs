16f77571179473db05657cc5788396f1f89e6311
Jan Snopkowski <jan.snopkowski@teb.edu.pl>
2024-09-16 12:33:57 +0200

    wprowadzenie wszystkich kostek z wynikiem

--- Kostka/MainPage.xaml.cs
+++ Kostka/MainPage.xaml.cs
@@ -13,8 +13,25 @@
         {
             //cos dodaje okok
             Random r = new Random();
-            int diceRoll = r.Next(1, 7);
-            CounterBtn.Text = "Wynik rzutu: " + diceRoll.ToString();
+            int diceRoll = 6;
+            if (K4.IsChecked)
+            {
+                diceRoll = r.Next(1, 5);
+            }
+            if (K6.IsChecked)
+            {
+                diceRoll = r.Next(1, 7);
+            }
+            if (K10.IsChecked)
+            {
+                diceRoll = r.Next(1, 11);
+            }
+            if (K12.IsChecked)
+            {
+                diceRoll = r.Next(1, 13);
+            }
+
+            RollLabel.Text = "Wynik rzutu: " + diceRoll.ToString();
         }
     }
 
