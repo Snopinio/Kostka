57b9df5c431f181db5e3537cf412cd47a5fcb469
1797007 <1797007@TE-GDA-D-D21E14>
2024-09-16 11:41:15 +0200

    Dodaj pliki projektów.

---
+++ Kostka/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace Kostka
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int diceRoll = r.Next(1, 7);
+            CounterBtn.Text = "Wynik rzutu: " + diceRoll.ToString();
+        }
+    }
+
+}
